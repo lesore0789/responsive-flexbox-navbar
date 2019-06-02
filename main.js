eaac25aaac141d823e5d8157bf62eb40d8ef65e9
lesore0789 <lesore0789@gmail.com>
2019-06-02 02:29:47 -0500

    Add js functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+let menuNav = document.getElementById('menu');
+let navToggle = document.getElementById('navbar-toggle');
+
+navToggle.addEventListener('click', function () {
+    
+    menuNav.classList.toggle('active');
+});
